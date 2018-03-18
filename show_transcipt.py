235de9b7a19d3d13605fe476080d133644eed53e
theschmocker <jacobcs1996@gmail.com>
2018-03-18 09:41:09 -0500

    will become module to access all transcripts

---
+++ show_transcipt.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+seasons = [
+    's1',
+    's2',
+    's3',
+    's4',
+    's5',
+    's6',
+    's7',
+]
+
+def getLines(pathToFile):
+    lines = []
+    with open(pathToFile) as f:
+
