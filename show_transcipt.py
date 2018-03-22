733638b946fbbcd06f4ea3c92665e820c1fef3bc
theschmocker <jacobcs1996@gmail.com>
2018-03-21 22:19:38 -0500

    work on module to load transcripts from filesystem

--- show_transcipt.py
+++ show_transcipt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import paths, os
+
 seasons = [
     's1',
     's2',
@@ -13,4 +15,32 @@ seasons = [
 def getLines(pathToFile):
     lines = []
     with open(pathToFile) as f:
+        lines = [line.replace('\n', '') for line in f.readlines()]
+    return lines
 
+def getTranscriptsForSeason(season):
+    seasonPath = paths.getSeasonPath(season)
+    # Ensure that episodes are in the right order
+    filePaths = sorted(os.listdir(seasonPath), key=lambda x: int(x.replace('.txt', '')))
+    filePaths = [os.path.join(seasonPath, f) for f in filePaths]
+    return filePaths
+
+def getLinesInSeason(season):
+    return [getLines(f) for f in getTranscriptsForSeason(season)]
+
+def getAllLines():
+    allLines = {}
+    for season in seasons:
+        allLines[season] = getLinesInSeason(season)
+    return allLines
+
+allLines = getAllLines()
+
+listLines = ''
+for season in seasons:
+    for episode in allLines[season]:
+        listLines += ' '.join(episode)
+
+
+print(listLines.count('\x96'))
+#print([ord(c) for c in listLines])
